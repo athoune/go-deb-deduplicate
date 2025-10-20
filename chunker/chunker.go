5c6bd392d100b7be8ca51e4652af148b7c5b0b95
Mathieu Lecarme <mathieu@garambrogne.net>
2025-10-20 17:42:12 +0200

    chunks go to the warehouse.

--- chunker/chunker.go
+++ chunker/chunker.go
@@ -2,37 +2,29 @@ package chunker
 
 import (
 	"crypto/sha256"
-	"encoding/hex"
-	"errors"
-	"fmt"
 	"io"
 	"log"
-	"os"
 
 	chunkers "github.com/PlakarKorp/go-cdc-chunkers"
 	_ "github.com/PlakarKorp/go-cdc-chunkers/chunkers/fastcdc"
-	"github.com/klauspost/compress/zstd"
+	"github.com/athoune/go-deb-deduplicate/warehouse"
 )
 
-type Chunker struct {
-	dir string
-}
+const CHUNK_MIN_SIZE = 10 * 1000
+const CHUNK_NORMAL_SIZE = 100 * 1000
+const CHUNK_MAX_SIZE = 5 * 1000 * 1000
 
-func New(dir string) *Chunker {
-	return &Chunker{dir}
-}
-
-func (c Chunker) Chunk(rd io.Reader) error {
-	chunker, err := chunkers.NewChunker("fastcdc", rd,
-		&chunkers.ChunkerOpts{})
+func ChunkAndStore(reader io.Reader, transaction *warehouse.Transaction) error {
+	chunker, err := chunkers.NewChunker("fastcdc", reader,
+		&chunkers.ChunkerOpts{
+			MinSize:    CHUNK_MIN_SIZE,
+			NormalSize: CHUNK_NORMAL_SIZE,
+			MaxSize:    CHUNK_MAX_SIZE,
+		})
 	if err != nil {
 		return err
 	}
-	root, err := os.OpenRoot(c.dir)
-	if err != nil {
-		return err
-	}
-	offset := 0
+
 	for {
 		chunk, err := chunker.Next()
 		if err != nil {
@@ -46,33 +38,10 @@ func (c Chunker) Chunk(rd io.Reader) error {
 		}
 		hasher := sha256.New()
 		hasher.Write(chunk)
-		h := hex.EncodeToString(hasher.Sum(nil))
-		_, err = root.Stat(h)
+		err = transaction.Set(hasher.Sum(nil), chunk)
 		if err != nil {
-			if errors.Is(err, os.ErrNotExist) {
-				f, err := root.OpenFile(h,
-					os.O_WRONLY|os.O_CREATE, 0640)
-				if err != nil {
-					return err
-				}
-				defer f.Close()
-				zw, err := zstd.NewWriter(f)
-				if err != nil {
-					return err
-				}
-				defer zw.Close()
-				_, err = zw.Write(chunk)
-				if err != nil {
-					return err
-				}
-			} else {
-				return err
-			}
+			log.Fatal(err)
 		}
-		chunkLen := len(chunk)
-		fmt.Println(offset, chunkLen)
-
-		offset += chunkLen
 	}
 	return nil
 }
