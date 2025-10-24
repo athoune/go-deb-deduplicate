8da33d03e0755c63b5d975c41260afae09b984e1
Mathieu Lecarme <mathieu@garambrogne.net>
2025-10-24 11:37:22 +0200

    feat: what do I need to upgrade my package ?

--- deduplicate/similarity.go
+++ deduplicate/similarity.go
@@ -22,3 +22,17 @@ func (t *Transaction) Similarity(old, fresh string) (*Similarity, error) {
 		Chunks: float64(len(u)) / float64(len(b)),
 	}, nil
 }
+
+func (t *Transaction) PatchRatio(old, fresh string) (float64, error) {
+	a, err := t.debManager.PathToChunks(old)
+	if err != nil {
+		return 0, err
+	}
+	b, err := t.debManager.PathToChunks(fresh)
+	if err != nil {
+		return 0, err
+	}
+
+	d := _bytes.WhatsUp(a, b, 32)
+	return float64(len(d)) / float64(len(b)), nil
+}
